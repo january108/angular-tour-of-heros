e5f3cc9b11b9bb8e3298c2a86b5a045fcc93d715
january108 <january1122@gmail.com>
2018-11-25 10:49:14 +0900

    dashboard

--- src/app/app-rounting.module.ts
+++ src/app/app-rounting.module.ts
@@ -1,9 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HerosComponent } from './heros/heros.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HerosComponent }
+  { path: 'heroes', component: HerosComponent },
+  { path: '', redirectTo: '/dashboard' , pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
